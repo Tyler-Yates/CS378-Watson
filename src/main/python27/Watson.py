8fc7627e9f204866bca9bcf28da8dd1ac9b42497
Tyler Yates <tyler.yates@utexas.edu>
2015-09-06 16:30:36 -0500

    Add initial python 2.7 implementation

---
+++ src/main/python27/Watson.py
@@ -0,0 +1,31 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import sys
+
+
+# Asks Watson a question using the given credentials
+#
+# argument 1: username
+# argument 2: password
+# argument 3: question
+def main():
+    response = ask_question(sys.argv[1], sys.argv[2], sys.argv[3])
+    print(json.dumps(json.loads(response.content), indent=4))
+
+
+# Returns an HTTP response after asking Watson the given question using the given credentials
+def ask_question(username, password, question):
+    authentication = HTTPBasicAuth(username, password)
+    headers = {'X-SyncTimeout': '30', 'Accept': 'application/json', 'Content-Type': 'application/json'}
+    data = {'question': {'questionText': question}}
+
+    response = requests.post('https://watson-wdc01.ihost.com/instance/540/deepqa/v1/question',
+                             data=json.dumps(data),
+                             auth=authentication,
+                             headers=headers)
+
+    return response
+
+
+main()
